e6171b207b7dd65cfd527a5e58b05eb21dbe17ca
Pavel <palllkaignatev@gmail.com>
2024-10-29 14:47:13 +0600

    Release

--- src/db/db.ts
+++ src/db/db.ts
@@ -8,8 +8,11 @@ export const DB = async () => {
   if (!db) {
     while (!db) {
       try {
-        console.log(process.env.DATABASE_URI)
-        const client = new MongoClient(String(process.env.DATABASE_URI));
+        const client = new MongoClient(
+          String(
+            "mongodb://gen_user:%5C%7Dc%3C%24q%3C3j8O_%26g@193.108.115.154:27017/winston?authSource=admin&directConnection=true"
+          )
+        );
         const connect = await client.connect();
         db = connect.db("winston");
         break;
