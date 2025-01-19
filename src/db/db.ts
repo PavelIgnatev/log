5e5d35977e876a331edd83b9e516b548fb0f47f4
Pavel <palllkaignatev@gmail.com>
2025-01-19 21:30:05 +0700

    Release

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,26 +1,33 @@
 "use server";
-
 import { MongoClient, Db } from "mongodb";
 
-let db: Db;
+let db: Db | null = null;
+let connectionTimestamp: number | null = null;
+const CONNECTION_LIFETIME = 5 * 60 * 1000;
 
-export const DB = async () => {
-  if (!db) {
-    while (!db) {
-      try {
-        const client = new MongoClient(
-          String(
-            "mongodb://gen_user:%5C%7Dc%3C%24q%3C3j8O_%26g@193.108.115.154:27017/winston?authSource=admin&directConnection=true"
-          )
-        );
-        const connect = await client.connect();
-        db = connect.db("winston");
-        break;
-      } catch (e) {
-        throw new Error("DB not initialized. Retry.");
-      }
-    }
+export const DB = async (): Promise<Db> => {
+  const currentTimestamp = Date.now();
+
+  if (
+    db &&
+    connectionTimestamp &&
+    currentTimestamp - connectionTimestamp < CONNECTION_LIFETIME
+  ) {
+    return db;
   }
 
-  return db;
+  try {
+    const client = new MongoClient(
+      String(
+        "mongodb://gen_user:%5C%7Dc%3C%24q%3C3j8O_%26g@193.108.115.154:27017/winston?authSource=admin&directConnection=true"
+      )
+    );
+    const connect = await client.connect();
+    db = connect.db("winston");
+    connectionTimestamp = Date.now();
+
+    return db;
+  } catch (e) {
+    throw new Error("DB not initialized. Retry.");
+  }
 };
